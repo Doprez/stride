75f8bbd13ea7bd094e74c3a8585dc4df6b618974
Doprez <73259914+Doprez@users.noreply.github.com>
2025-05-22 09:59:28 -0600

    use gamebuilder

---
+++ sources/Leap/Leap/Extensions/GameBuildExtensions.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap.Extensions;
+public static class GameBuildExtensions
+{
+    public static IGameBuilder UseDefaultGameSystems(this IGameBuilder gameBuilder)
+    {
+        var services = gameBuilder.Services[typeof(IServiceRegistry)] as IServiceRegistry;
+
+        var scriptSystem = new ScriptSystem(services);
+        var sceneSystem = new SceneSystem(services);
+        var audioSystem = new AudioSystem(services);
+        var gameFontSystem = new GameFontSystem(services);
+        var spriteAnimationSystem = new SpriteAnimationSystem(services);
+        var debugTextSystem = new DebugTextSystem(services);
+        var gameProfilingSystem = new GameProfilingSystem(services);
+        var inputSystem = new InputSystem(services);
+        var effectSystem = new EffectSystem(services);
+        // registers itself as a service
+        var streamingManager = new StreamingManager(services);
+
+        gameBuilder
+            .AddGameSystem(scriptSystem)
+            .AddGameSystem(sceneSystem)
+            .AddGameSystem(audioSystem)
+            .AddGameSystem(gameFontSystem)
+            .AddGameSystem(spriteAnimationSystem)
+            .AddGameSystem(debugTextSystem)
+            .AddGameSystem(gameProfilingSystem)
+            .AddGameSystem(inputSystem)
+            .AddGameSystem(effectSystem)
+            .AddGameSystem(streamingManager);
+
+        // add services
+        gameBuilder
+            .AddService(scriptSystem)
+            .AddService(sceneSystem)
+            .AddService(spriteAnimationSystem)
+            .AddService(debugTextSystem)
+            .AddService(gameProfilingSystem)
+            .AddService(inputSystem)
+            .AddService(effectSystem)
+            .AddService(inputSystem.Manager)
+            .AddService(audioSystem)
+            .AddService<IAudioEngineProvider>(audioSystem)
+            .AddService(gameFontSystem)
+            .AddService(gameFontSystem.FontSystem)
+            .AddService<IFontFactory>(gameFontSystem.FontSystem);
+
+        return gameBuilder;
+    }
+
+    public static IGameBuilder UseDefaultGameSystemsDI(this IGameBuilder gameBuilder)
+    {
+        gameBuilder
+            .AddService<ScriptSystem>()
+            .AddService<SceneSystem>()
+            .AddService<SpriteAnimationSystem>()
+            .AddService<DebugTextSystem>()
+            .AddService<GameProfilingSystem>()
+            .AddService<EffectSystem>()
+            .AddService<StreamingManager>()
+            .AddService<IAudioEngineProvider, AudioSystem>();
+
+        return gameBuilder;
+    }
+
+
+}
--- sources/Leap/Leap/Program.cs
+++ sources/Leap/Leap/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Doprez.Stride.Input;
+using GameBuilderTest;
+using Stride.Core.Diagnostics;
+using Stride.Core.IO;
+using Stride.Core.Serialization.Contents;
+using Stride.Engine;
+using Stride.Engine.Builder;
+using Stride.Engine.Design;
+using Stride.Games;
+using Stride.Rendering;
+
+var gameBuilder = GameBuilder.Create();
+
+gameBuilder.UseDefaultGameSystemsDI()
+    .UseStrideInput()
+    .UseStrideFontSystem()
+    .UseDefaultDb()
+    .UseDefaultContentManager()
+    .SetGameContext(GameContextFactory.NewGameContextSDL())
+    //.AddStrideInputSource(new SharpHookInputSource())
+    //.AddStrideInputSource(new HIDDeviceInputSource())
+    .AddLogListener(new ConsoleLogListener());
+
+var game = gameBuilder.Build();
+
+var content = game.Services.GetService<IContentManager>();
+var settings = content.Load<GameSettings>("GameSettings");
+
+var sceneSystem = game.Services.GetService<SceneSystem>();
+sceneSystem.InitialSceneUrl = settings.DefaultSceneUrl;
+sceneSystem.InitialGraphicsCompositorUrl = settings.DefaultGraphicsCompositorUrl;
+
+var fileProvider = game.Services.GetService<IDatabaseFileProviderService>().FileProvider;
+game.Services.GetService<EffectSystem>()
+    .CreateDefaultEffectCompiler(fileProvider);
+
+game.Run();
